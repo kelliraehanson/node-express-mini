2f2ff2cf581b484250ccc1894941fcdaffc8cbb0
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-12 10:02:54 -0600

    morning commit

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ server.post("/api/users", (req, res) => {
 server.get("/api/users", (req, res) => {
     db.find()
     .then(users => {
-      res.json(users)
+      res.status(200).json(users);
     })
-    .catch(() => {
+    .catch(err => {
         res.status(500).json({ error: "The users information could not be retrieved." })
     })
   });
@@ -56,17 +56,17 @@ server.get("/api/users", (req, res) => {
   server.get("/api/users/:id", (req, res) => {
     const id  = req.params.id; //or could be db.findById(req.params.id)
     db.findById(id)
-    .then((user) => {
+    .then(user => {
         // this says to either return the user id or the 404 message
-        if (user.length === 0) {
-            return res.status(404).json({ message: "The user with the specified ID does not exist." });
-        } else
-        console.log(user);
-        res.status(200).json(user); // If there is a user send the user/response.
+        if (user) {
+            res.status(200).json(user);
+        } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." }) // If there is a user send the user/response.
+        }
     })
-    .catch(() =>
-    res.status(404)
-    .json({ message: "The user with the specified ID does not exist." })
+    .catch(err =>
+    res.status(500)
+    .json({ error: "The users information could not be retrieved." })
     )
 })
 
@@ -76,60 +76,53 @@ server.get("/api/users", (req, res) => {
 // ============================ DELETE requests
 
 server.delete("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    db.remove(id)
-      .then(user => {
-          if(!user) {
-              res.status(404)
-              .json({ message: "The user with the specified ID does not exist." })
-          } else {
-              res.json("Deleted")
-          }
-        })
-        .catch(err => {
-            res.status(500).json({ error: "The user could not be removed" })
-        })
+    userId = req.params.id;
+    db.remove(userId)
+      .then(deleted => {
+        if (deleted) {
+          db.find()
+            .then(users => res.status(200).json(users))
+            .catch(() => res.status(500).json({ error: "Error" }));
+        } else {
+          res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The user could not be removed." });
+      });
   });
 
   // ================================================================== End DELETE requests
 
   // =========================================== PUT requests
 
-//   When the client makes a `PUT` request to `/api/users/:id`:
-
-// - If the _user_ with the specified `id` is not found:
-
-//   - return HTTP status code `404` (Not Found).
-//   - return the following JSON object: `{ message: "The user with the specified ID does not exist." }`.
-
-// - If the request body is missing the `name` or `bio` property:
-
-//   - cancel the request.
-//   - respond with HTTP status code `400` (Bad Request).
-//   - return the following JSON response: `{ errorMessage: "Please provide name and bio for the user." }`.
-
-// - If there's an error when updating the _user_:
-
-//   - cancel the request.
-//   - respond with HTTP status code `500`.
-//   - return the following JSON object: `{ error: "The user information could not be modified." }`.
-
-// - If the user is found and the new information is valid:
-
-//   - update the user document in the database using the new information sent in the `reques body`.
-//   - return HTTP status code `200` (OK).
-//   - return the newly updated _user document_.
-
-// server.put('/api/users/:id', (req,res) => {
-//     const { id } = req.params;
-//     const {name, bio} = req.body;
-//     const user = { name, bio };
-//     db.update(id , user)
-//     .then(count => {
-
-//     })
-    
-// })
+server.put("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    if (!changes.name || !changes.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+      db.update(id, changes)
+        .then(user => {
+          if (user) {
+            db.find()
+              .then(users => res.status(200).json(users))
+              .catch(() => res.status(500).json({ error: "Error" }));
+          } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
+          }
+        })
+        .catch(() =>
+          res
+            .status(500)
+            .json({ error: "The user information could not be modified." })
+        );
+    }
+  });
 
 //======================================================================== End PUT requests
 
